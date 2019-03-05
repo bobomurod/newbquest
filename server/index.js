f5248d2e04a5e49f57a2a46e6bf92f23b30d66ac
bobomurod <bobomurod.boboev@yandex.ru>
2019-03-05 14:59:09 +0500

    all about mainstream

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: '🦄🌈✨Hello World! 🌈✨🦄'
+  });
+});
+
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error('Not Found - ' + req.originalUrl);
+  next(error);
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: err.stack
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
