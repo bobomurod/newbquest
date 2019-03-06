4fa92f43c547f309d99eac059490bae2fea4503b
bobomurod <bobomurod.boboev@yandex.ru>
2019-03-07 03:48:19 +0500

    VUEjs inited and we where have lots of fun

---
+++ client/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home text-center">
+    <div class="jumbotron">
+  <h1 class="display-3">Auth for MIDO!</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>We try our best make today better than yesterday.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Sign Up!</a>
+  </p>
+</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+};
+</script>
