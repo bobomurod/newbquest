c6a77bdb91cf5d2f073ef3134effb5fccb706391
bobomurod <bobomurod.boboev@yandex.ru>
2019-03-07 22:22:15 +0500

    Simple page

---
+++ client/src/views/SimplePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>
+            Simple Page
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'simplepage'
+}
+</script>
+
+<style>
+
+</style>
