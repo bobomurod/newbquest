3513410cbb83fa4efb9525a93e9edd76194e5fa8
bobomurod <bobomurod.boboev@yandex.ru>
2019-03-15 18:36:30 +0500

    Dashboard

---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <section>
+        <h1>Dashboard</h1>
+        <form action="">
+
+        </form>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Dashboard',
+}
+</script>
+
+<style>
+
+</style>
