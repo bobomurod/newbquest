311c0d541f1255ce5cd87810f34fe67146cc3088
bobomurod <bobomurod.boboev@yandex.ru>
2019-03-31 16:39:45 +0500

    setting up middleware checkers

--- server/auth/middlewares.js
+++ server/auth/middlewares.js
@@ -1,1 +1,8 @@
-function checkTokenSetUser() {}
+function checkTokenSetUser(req, res, next) {
+  console.log("Ok, this is middleware");
+  console.log("i will run for every request comes to server");
+  console.log("if i call NEXT,  i will go to the next middleware");
+  next();
+}
+
+module.exports = checkTokenSetUser;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
-//begin
+//bismillah
 
-const express = require('express');
-const volleyball = require('volleyball');
-const bcrypt = require('bcryptjs')
-const cors = require('cors');
+const express = require("express");
+const volleyball = require("volleyball");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
+const middlewares = require("./auth/middlewares");
 
-require('dotenv').config({path: './.env'});
+require("dotenv").config({ path: "./.env" });
 
 //const bodyParser = require('body-parser');
 
@@ -14,33 +15,37 @@ require('dotenv').config({path: './.env'});
 
 const app = express();
 
-const auth = require('./auth/index.js');
-const products = require('./products/index.js');
+const auth = require("./auth/index.js");
+const products = require("./products/index.js");
+
+app.use(middlewares);
 
 app.use(volleyball);
-app.use(cors({
-  origin: 'http://localhost:8080'
-}));
+app.use(
+  cors({
+    origin: "http://localhost:8080"
+  })
+);
 // app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded( { extended: false } ))
 
 //app.use(morgan('dev'));
 //app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    message: 'Hello World! '
+    message: "Hello World! "
   });
 });
 
-app.use('/auth', auth);
-app.use('/products', products);
+app.use("/auth", auth);
+app.use("/products", products);
 
 function notFound(req, res, next) {
   res.status(404);
-  const error = new Error('Not Found - ' + req.originalUrl);
+  const error = new Error("Not Found - " + req.originalUrl);
   next(error);
 }
 
@@ -57,5 +62,5 @@ app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log('Listening on port', port);
+  console.log("Listening on port", port);
 });
