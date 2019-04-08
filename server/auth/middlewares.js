7340e0de0774e13c6e04309028955880315abf55
bobomurod <bobomurod.boboev@yandex.ru>
2019-04-08 22:58:05 +0500

    Token generating screens update

--- server/auth/middlewares.js
+++ server/auth/middlewares.js
@@ -6,11 +6,16 @@ function checkTokenSetUser(req, res, next) {
     const token = authHeader.split(" ")[1];
     console.log(token);
     if (token) {
-      webToken.verify(token, process.env.TOKEN_SECRET, function(err, decoded) {
-        console.log(decoded.username);
+      webToken.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+        if(err){
+          console.log(err);
+        } else {
+          console.log(user);
+          req.user = user;
+          next();
+        }
       });
-    } else {
-    }
+    } 
   } else {
     next();
   }
