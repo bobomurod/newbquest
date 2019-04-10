a6d479f4254ba4f3cb9c0524cc1ddebc0ab2e93f
bobomurod <bobomurod.boboev@yandex.ru>
2019-04-10 22:40:13 +0500

    forget

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,6 +1,8 @@
 <template>
     <section>
         <h1>Dashboard</h1>
+        <!-- <h1 v-if="!user"> Requesting... </h1> -->
+        <h1>Hello {{user.username}}</h1>
         <form action="">
 
         </form>
@@ -12,13 +14,31 @@
 </template>
 
 <script>
+
+const API_URL = 'http://localhost:5000/'
+
 export default {
   name: 'Dashboard',
+  data: ()=>({
+    isLoading: true,
+    user: null
+  }),
   methods: {
     logout() {
       localStorage.clear();
       this.$router.push('/');
     },
+  },
+  mounted(){
+    fetch(API_URL, {
+      headers: {
+        authorization: `Bearer ${localStorage.token}`
+      }
+    }).then(res => res.json())
+      .then((result) => {
+        console.log(result)
+        this.user = result.user
+      })
   },
 
 };
