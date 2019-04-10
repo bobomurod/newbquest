9d066ac9b75a3f54e64fb0ec8cb56296b07ec254
bobomurod <bobomurod.boboev@yandex.ru>
2019-04-10 22:39:30 +0500

    subdomain console log

--- client/src/views/SimplePage.vue
+++ client/src/views/SimplePage.vue
@@ -3,12 +3,32 @@
         <h1>
             Simple Page
         </h1>
+        <div class="form-group">
+          <label for="">Interactive form</label>
+          <input 
+            type="text" 
+            name="" id="" 
+            class="form-control" 
+            placeholder="" 
+            aria-describedby="helpId" 
+            @keyDown="typing()"
+            >
+          <small id="helpId" class="text-muted">Help text</small>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
   name: 'simplepage',
+  data: () => ({
+      interactiveInput: null
+  }),
+  methods: {
+      typing(){
+          console.log("typing...")
+      }
+  }
 };
 </script>
 
