698bf117010f8e788e3dd85856b0598a82bc4363
bobomurod <bobomurod.boboev@yandex.ru>
2019-04-24 00:14:32 +0500

    унаследование объекта с базы

--- mido/src/views/Home.vue
+++ mido/src/views/Home.vue
@@ -2,6 +2,12 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>{{this.shop.name}}</h2>
+    <h2>{{this.shop.title}}</h2>
+    <h2>{{this.shop.about}}</h2>
+    <h2>{{this.shop.products}}</h2>
+    <h2>{{this.shop.contacts}}</h2>
+
   </div>
 </template>
 
@@ -21,6 +27,7 @@ export default {
       title: null,
       about: null,
       products: [],
+      contacts: null,
     }
       }),
   components: {
@@ -30,7 +37,10 @@ export default {
     console.log(this.host[0])
     fetch(API_URL + this.host[0])
       .then(raw_result => raw_result.json())
-      .then(result => console.log(result))
+      .then((result) => {
+        this.shop = result;
+        console.log(result)
+      })
   }
 };
 </script>
