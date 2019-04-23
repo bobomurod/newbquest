1cae31a6abeb39081983caf795268cc2ef9fb902
bobomurod <bobomurod.boboev@yandex.ru>
2019-04-23 23:51:20 +0500

    Shop generating...

---
+++ client/src/views/ShopGenerator.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Generating shoppp...</h1>
+    <input type="text"> </input>
+    <button v-on:click="generate" >hello</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Generator',
+    methods: {
+        generate(){
+            console.log("hello")
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
