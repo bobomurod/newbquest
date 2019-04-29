fe53e1e609a3c7ff1c3b6d8e0a44e856daefe3a8
bobomurod <bobomurod.boboev@yandex.ru>
2019-04-30 00:08:48 +0500

    proper route config

---
+++ client/src/views/UsersList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="">
+        <h1>
+            userslist
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'userslist',
+    created(){
+        console.log('loaded')
+    },
+    mounted(){
+        console.log('mounted')
+    },
+    updated(){
+        console.log('updated')
+    },
+    destroyed(){
+        console.log('destroyed')
+    }
+}
+</script>
+
+<style>
+
+</style>
