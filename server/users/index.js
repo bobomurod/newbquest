6d5e753ee6bd2c226e8de7a1c93b5f9f44bc6d98
bobomurod <bobomurod.boboev@yandex.ru>
2019-05-03 00:06:41 +0500

    users modules db init

--- server/users/index.js
+++ server/users/index.js
@@ -1,3 +1,10 @@
 //we love our users
 const db = require('../db/connection');
 const users = db.get('users');
+
+const express = require('express');
+const router = express.Router();
+
+
+
+module.exports = router;
