beed01aadd86c2e87ce3655811612e921f6101b3
bobomurod <bobomurod.boboev@yandex.ru>
2019-05-10 00:03:35 +0500

    material design font and icons installd

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,18 @@
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vue from 'vue';
 import Vuetify from 'vuetify'
+// import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
 import 'bootswatch/dist/litera/bootstrap.css';
+import '../node_modules/vuetify/dist/vuetify.min.css';
+import '@mdi/font/css/materialdesignicons.css';
 import App from './App.vue';
 import router from './router';
-import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  icons: {
+    iconfont: 'mdiSvg',
+  },
+});
 Vue.config.productionTip = false;
 
 new Vue({
