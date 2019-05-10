d39a1efd78b9a594df92649410b96841ffcb9b5f
bobomurod <bobomurod.boboev@yandex.ru>
2019-05-11 00:03:36 +0500

    fontawesome icons installed

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,4 @@
-import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/css/all.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vue from 'vue';
 import Vuetify from 'vuetify'
@@ -6,8 +6,14 @@ import Vuetify from 'vuetify'
 import 'bootswatch/dist/litera/bootstrap.css';
 import '../node_modules/vuetify/dist/vuetify.min.css';
 import '@mdi/font/css/materialdesignicons.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue';
 import router from './router';
+
+Vue.component('font-awesome-icon', FontAwesomeIcon) // Register component globally
+library.add(fas)
 
 Vue.use(Vuetify, {
   icons: {
