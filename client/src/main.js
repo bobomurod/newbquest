d3279a8da8d57cae6fcf8e371f286995153f9a07
bobomurod <bobomurod.boboev@yandex.ru>
2019-05-12 01:54:54 +0500

    vuetify 4

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@ import Vuetify from 'vuetify'
 import 'bootswatch/dist/litera/bootstrap.css';
 import App from './App.vue';
 import router from './router';
+import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
 
 Vue.use(Vuetify)
 Vue.config.productionTip = false;
