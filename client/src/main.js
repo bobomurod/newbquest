05133121da4e5a89360985a62bba2aea88cedf91
bobomurod <bobomurod.boboev@yandex.ru>
2019-05-12 01:52:26 +0500

    vuetify 3

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify'
 import 'bootswatch/dist/litera/bootstrap.css';
 import App from './App.vue';
 import router from './router';
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false;
 
 new Vue({
